f4254babb7ca4c789bae9a7cc5edf3fce23dfd5d
EnoughKK <ksu8063@naver.com>
2022-06-16 01:24:56 +0900

    +mapAPI

--- kakao.js
+++ kakao.js
@@ -7,6 +7,11 @@ async function crawling() {
     await page.goto("https://place.map.kakao.com/8601238");
     await page.waitFor('h2.tit_location');
     let ehList = await page.$$("#mArticle");
+    let menu = await page.$$("span.loss_word");
+    menu.forEach(async menu => {
+        let text = await (await menu.getProperty("innerText")).jsonValue();
+        console.log(await text);
+    })
 
     for(let eh of ehList){
         let title = await eh.$eval('h2.tit_location', function(el){
@@ -21,19 +26,14 @@ async function crawling() {
         let contact = await eh.$eval('span.txt_contact', function(el){
             return el.innerText
         })
-        let manu = await eh.$eval('span.loss_word', function(el){
+        /*let menu = await eh.$$eval('span.loss_word', function(el){
             return el.innerText
-        })
+        })*/
         let imageURL = await eh.$eval('.link_photo', function(el){
             return window.getComputedStyle(el).backgroundImage.match(/url\("(.*)"/)[1]
         })
 
-        fs.appendFile('result.csv', `${title}, ${eval}, ${address}, ${contact}, ${manu}, ${imageURL}\n`,function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-        });
-
-        console.log(title, eval, address, contact, manu, imageURL);
+        console.log(title, eval, address, contact, imageURL);
     }
     browser.close();
 }
